822352d2358f31e88d8cfba43b02088abcf4720a
Chris Brinson <chrisjbrinson@gmail.com>
2016-05-16 22:07:22 -0600

    Initial

---
+++ wiki.js
@@ -0,0 +1,42 @@
+
+$(document).ready(function(){
+
+	var title, url;
+    var api = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
+    var cb = '&callback=?';
+	
+
+	function click(){
+			title = document.getElementById('wiki').value;
+			url = api + title + cb;
+			retrieve();
+	}
+	
+
+	function retrieve(){
+		var arrayTitle = [];
+		var arrayDesc = [];
+		var arrayID = [];
+		$.getJSON(url, function(json){
+			console.log(json);
+			$.each(json.query.pages, function(index, item) {
+				arrayTitle.push(item.title);
+				arrayDesc.push(item.extract);
+				arrayID.push(item.pageid);
+			});	
+			console.log(arrayID);
+			$('li').each(function(x){
+				var url = "http://en.wikipedia.org/?curid=" + arrayID[x];
+				$(this).html("<a href='"+url+"'>" + arrayTitle[x] + ": " + arrayDesc[x]);
+				
+			});
+			
+		});
+		
+	}
+
+	$("#getWiki").on("click", function(){
+		click();
+	});
+
+});
