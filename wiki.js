d4560be39425d5f86ed3ff3bc073f88c0d53a2a1
Chris Brinson <chrisjbrinson@gmail.com>
2016-05-31 19:37:30 -0600

    Fixing stuff
    Searching on keypress, wiki logo

--- wiki.js
+++ wiki.js
@@ -5,7 +5,10 @@ $(document).ready(function(){
     var apiURL = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
     var cb = '&callback=?';
     var urlRandom = "https://en.wikipedia.org/wiki/Special:Random";
-	
+	var arrayTitle = [];
+	var arrayDesc = [];
+	var arrayID = [];
+
 
 	function click(){
 		title = document.getElementById('wiki').value;
@@ -13,11 +16,7 @@ $(document).ready(function(){
 		retrieve();
 	}
 
-	
 	function retrieve(){
-		var arrayTitle = [];
-		var arrayDesc = [];
-		var arrayID = [];
 		$.getJSON(finalURL, function(json){
 			console.log(json);
 			$.each(json.query.pages, function(index, item) {
@@ -25,16 +24,30 @@ $(document).ready(function(){
 				arrayDesc.push(item.extract);
 				arrayID.push(item.pageid);
 			});	
-			console.log(arrayID);
+			
 			$('li').each(function(x){
 				var url = "http://en.wikipedia.org/?curid=" + arrayID[x];
 				$(this).html("<a href='"+url+"' target='_blank'>" + "<h1>" + arrayTitle[x] + "</h1>" + arrayDesc[x]);
-				
+
 			});
+			arrayTitle = [];
+			arrayDesc = [];
+			arrayID = [];
 			
 		});
 		
 	}
+	$('li').hover(function(){
+		$(this).css("background","#d9d9d9");
+		}, function(){
+			$(this).css("background", "");
+		});
+
+	$("#wiki").keypress(function(event){
+		if(event.keyCode == 13){
+		click();
+		} 	
+	});
 
 	$("#getWiki").on("click", function(){
 		click();
